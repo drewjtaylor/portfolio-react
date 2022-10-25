83ae8f10eb6e395d372b3184b12358ad9f1131a2
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-25 12:15:37 -0400

    Navbar started

--- src/components/PortfolioNavbar.js
+++ src/components/PortfolioNavbar.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import logo from '../img/initials-black.png';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import {Link} from 'react-router-dom'
 
 const PortfolioNavbar = () => {
   return (
-    <div>PortfolioNavbar</div>
+    <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            Andrew Taylor
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+            <Nav.Link><Link to='projects'>My Projects</Link></Nav.Link>
+            <Nav.Link><Link to='contact'>Contact</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
   )
 }
 
