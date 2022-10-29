2a07b5b0bf51afdedcc0ef39f653d28751513e56
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-29 14:54:14 -0400

    Updated projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import projects from '../data/projects.js'
 // import ProjectCard from '../components/ProjectCard';
 
@@ -17,7 +18,7 @@ const ProjectsPage = () => {
                     <Row className='my-4'>
                         {/* Coloumn for title and img */}
                         <Col md={3}>
-                            <h2>{project.title}</h2>
+                            <h2><a href={project.liveUrl} target="_blank" rel='noreferrer'>{project.title}</a></h2>
                             {/* <img src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/> */}
                             <Image className='shadow' src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/>
                         </Col>
@@ -25,6 +26,7 @@ const ProjectsPage = () => {
                         <Col md={9}>
                             <h3><em>{project.subTitle}</em></h3>
                             <p>{project.description}</p>
+                            <Button as="a" href={project.githubUrl} target="_blank" rel='noreferrer' variant='success'>See it on Github</Button>
                         </Col>
                     </Row>
                     <hr />
