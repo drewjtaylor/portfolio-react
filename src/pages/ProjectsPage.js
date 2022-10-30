9caf1a4d6e8c1c694a4e3f522ad102315440b468
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-10-29 22:04:40 -0400

    Basic animation-need to DL spring

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -4,10 +4,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import projects from '../data/projects.js'
+
+import { useSPring, animated } from 'react-spring'; // Need to download react-spring from home
+
 // import ProjectCard from '../components/ProjectCard';
 
 const ProjectsPage = () => {
-  
+  const animatedProps = useSpring({ to: {opacity: 1}, from: {opacity: 0}})
   
   
   return (
@@ -15,20 +18,22 @@ const ProjectsPage = () => {
         <Row>
             {projects.map(project => {
                 return <>
-                    <Row className='my-4'>
-                        {/* Coloumn for title and img */}
-                        <Col md={3}>
-                            <h2><a href={project.liveUrl} target="_blank" rel='noreferrer'>{project.title}</a></h2>
-                            {/* <img src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/> */}
-                            <Image className='shadow' src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/>
-                        </Col>
-                        {/* column for description */}
-                        <Col md={9}>
-                            <h3><em>{project.subTitle}</em></h3>
-                            <p>{project.description}</p>
-                            <Button as="a" href={project.githubUrl} target="_blank" rel='noreferrer' variant='success'>See it on Github</Button>
-                        </Col>
-                    </Row>
+                    <animated.div style={animatedProps}>
+                        <Row className='my-4'>
+                            {/* Coloumn for title and img */}
+                            <Col md={3}>
+                                <h2><a href={project.liveUrl} target="_blank" rel='noreferrer'>{project.title}</a></h2>
+                                {/* <img src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/> */}
+                                <Image className='shadow' src={project.imgSource} fluid width='100%' alt={`${project.title} screenshot`}/>
+                            </Col>
+                            {/* column for description */}
+                            <Col md={9}>
+                                <h3><em>{project.subTitle}</em></h3>
+                                <p>{project.description}</p>
+                                <Button as="a" href={project.githubUrl} target="_blank" rel='noreferrer' variant='success'>See it on Github</Button>
+                            </Col>
+                        </Row>
+                    </animated.div>
                     <hr />
                 </>
             })}
