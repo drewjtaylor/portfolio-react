2d4f1986b5aad6aef210bce4ac9dc27b519e6d5a
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-11-16 21:24:55 -0500

    Contact info listed--needs styled

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const ContactPage = () => {
-  return (
-    <div>ContactPage</div>
+  return ( <>
+        <div>Phone: <a href="tel:7276977619">727-697-7619</a> (text or call)</div>
+        <div>LinkedIn: <a href="https://www.linkedin.com/in/andrew-taylor-703717248/">https://www.linkedin.com/in/andrew-taylor-703717248/</a></div>
+        <div>Email: <a href="mailto:drew.j.taylor1@gmail.com">drew.j.taylor1@gmail.com</a></div>
+    </>
   )
 }
 
